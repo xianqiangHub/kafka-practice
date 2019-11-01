2a3692de5dc06feb380ac3ca99c5445208bb7b13
xianqiang <982535901@qq.com>
2019-11-01 13:56:30 +0800

    测试暂停  poll阻塞

--- src/main/java/com/bigdata/DynamicConsumer.java
+++ src/main/java/com/bigdata/DynamicConsumer.java
@@ -20,21 +20,21 @@ public class DynamicConsumer {
 
 
         //起一个线程修改 topic
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-
-                try {
-                    Thread.sleep(10000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                // 变更为订阅topic： btopic， ctopic   在10s之后
-                concurrenQueue.addAll(Arrays.asList("btopic", "ctopic"));
-            }
-        };
-        //启动线程
-        new Thread(runnable).start();
+//        Runnable runnable = new Runnable() {
+//            @Override
+//            public void run() {
+//
+//                try {
+//                    Thread.sleep(10000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                // 变更为订阅topic： btopic， ctopic   在10s之后
+//                concurrenQueue.addAll(Arrays.asList("btopic", "ctopic"));
+//            }
+//        };
+//        //启动线程
+//        new Thread(runnable).start();
 
         Properties props = new Properties();
         props.put("bootstrap.servers", "hadoop102:9092,hadoop103:9092,hadoop104:9092");
@@ -49,31 +49,31 @@ public class DynamicConsumer {
         // 最开始的订阅列表：atopic、btopic
         consumer.subscribe(Arrays.asList("atopic", "btopic"));
 
-//        Runnable runnable = new Runnable() {
-//            @Override
-//            public void run() {
-//
-//                try {
-//                    Thread.sleep(10000);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//                // 在起个线程，直接修改订阅的topic
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                // 在起个线程，直接修改订阅的topic
 //                consumer.subscribe(Arrays.asList("btopic", "ctopic"));
-////                java.util.ConcurrentModificationException: KafkaConsumer is not safe for multi-threaded access
-//            }
-//        };
-//        //启动线程
-//        new Thread(runnable).start();
+//                java.util.ConcurrentModificationException: KafkaConsumer is not safe for multi-threaded access
+           consumer.wakeup();//让poll跳出循坏，类似于优雅的停止消费，抛出异常正常，finanly中加close（）方法，处理结束前的提交
+            }
+        };
+        //启动线程
+        new Thread(runnable).start();
 
 
         //flink为isrunning
         while (true) {
             //阻塞有数据就会拉过来，但是加上时间之后有机会轮询订阅状态是否更新
-            consumer.poll(Duration.ofSeconds(2));   //死循环，可以阻塞时间
+            consumer.poll(Duration.ofSeconds(2000000));   //死循环，可以阻塞时间
 //            consumer.wakeup();
 //            consumer.pause();
-
             System.out.println(consumer.subscription());
 
             //10s进入
@@ -86,6 +86,21 @@ public class DynamicConsumer {
                 concurrenQueue.clear();
                 consumer.subscribe(topics); // 重新订阅topic
             }
+
+            // commitSync() 将会提交由 poll() 返回的最新偏移量
+//            consumer.commitSync();    同步提交会阻塞，影响吞吐   失败会一直重试
+
+            //只管提交，不管是否成功，也有回调函数，一种情况小偏移量提交失败，大偏移量成功，尝试重新提交失败的小偏移量
+            // 这时候再平衡，重复消费
+//            consumer.commitAsync();   不会重试
+
+
+//            ConsumerRebalancelistener有两个需要实现的方法。
+//
+//            (1) public void onPartitionsRevoked(Collection<TopicPartition> partitions)方法会在 再均衡开始之前和消费者停止读取消息之后被调用。如果在这里提交偏移量，下一个接 管分区 的消费者就知道该从哪里开始读取了。
+//
+//            (2) public void onPartitionsAssigned(Collection<TopicPartition> partitions)方法会在 重新分配分区之后和消费者开始读取消息之前被调用。
+
 
         }
     }
